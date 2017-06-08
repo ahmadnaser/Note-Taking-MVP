5ab409024db51b77e03544f90eb279320cbd6880
zeerorg <rishabh4325@gmail.com>
2017-06-08 12:11:42 +0530

    Notes app using Kotlin

---
+++ app/src/main/java/com/example/zeerorg/firstkotlin/NotesItem.kt
@@ -0,0 +1,45 @@
+package com.example.zeerorg.firstkotlin
+
+import android.support.annotation.IdRes
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+import com.mikepenz.fastadapter.items.AbstractItem
+
+/**
+ * Created by zeerorg on 6/8/17.
+ */
+
+class NotesItem(val data: String = "") : AbstractItem<NotesItem, NotesItem.ViewHolder>() {
+
+//    constructor(data: String) {
+//
+//    }
+
+    @IdRes
+    override fun getType(): Int {
+        return R.id.note_item
+    }
+
+    override fun getLayoutRes(): Int {
+        return R.layout.note_item
+    }
+
+    override fun getViewHolder(v: View): ViewHolder { //removed null test to see
+        return ViewHolder(v)
+    }
+
+    override fun bindView(holder: ViewHolder, payloads: MutableList<Any>) { // removed null test to see
+        super.bindView(holder, payloads)
+        holder.data?.text = data
+    }
+
+    override fun unbindView(holder: ViewHolder?) {
+        super.unbindView(holder)
+        holder?.data?.text = null
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val data: TextView? = view.findViewById(R.id.note_data) as TextView
+    }
+}
